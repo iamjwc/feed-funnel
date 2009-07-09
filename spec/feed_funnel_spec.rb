287f323cced0c78757b6f57e83f277f14c679dd3
Justin Camerer <iamjwc@gmail.com>
2009-07-09 14:20:38 -0400

    Cleaning up some specs

--- spec/feed_funnel_spec.rb
+++ spec/feed_funnel_spec.rb
@@ -1,74 +1,35 @@
 require 'spec/spec_helper'
 
-describe FeedFunnel::LevenshteinFunnel do
+describe "With simple feeds" do
   before do
     @master_feed = FeedFunnel::Feed.new(File.read("spec/rss/super_simple_1.rss"))
     @other_feed  = FeedFunnel::Feed.new(File.read("spec/rss/super_simple_2.rss"))
 
-    @funnel_on_description = FeedFunnel::LevenshteinFunnel.new(@master_feed) {|i| (i.h % :description).inner_text }
+    @combined_feed_regex = /enclosure.*episode1.*enclosure.*media:group.*media:content.*episode1.*media:content.*media:content.*episode1.*media:content.*media:group/
   end
 
-  it "should be able to combine 2 simple feeds on a description" do
-    @funnel_on_description.funnel(@other_feed).to_s.should match(
-      /#{%w{
-        enclosure
-        episode1
-        enclosure
-        media:group
-        media:content
-        episode1
-        media:content
-        media:content
-        episode1
-        media:content
-        media:group
-      }.join('.*')}/
-    )
+  describe FeedFunnel::LevenshteinFunnel do
+    before do
+      @funnel_on_description = FeedFunnel::LevenshteinFunnel.new(@master_feed) {|i| (i.h % :description).inner_text }
+    end
+  
+    it "should be able to combine 2 simple feeds on a description" do
+      @funnel_on_description.funnel(@other_feed).to_s.should match(@combined_feed_regex)
+    end
   end
-end
-
-describe FeedFunnel::DirectMatchFunnel do
-  before do
-    @master_feed = FeedFunnel::Feed.new(File.read("spec/rss/super_simple_1.rss"))
-    @other_feed  = FeedFunnel::Feed.new(File.read("spec/rss/super_simple_2.rss"))
-
-    @funnel_on_filename_with_extension    = FeedFunnel::DirectMatchFunnel.new(@master_feed) {|i| (i.h % :enclosure)[:url] }
-    @funnel_on_filename_without_extension = FeedFunnel::DirectMatchFunnel.new(@master_feed) {|i| (i.h % :enclosure)[:url].gsub(/\..*$/, "") }
-  end
-
-  it "should be able to combine 2 simple feeds on a filename without extension" do
-    @funnel_on_filename_without_extension.funnel(@other_feed).to_s.should match(
-      /#{%w{
-        enclosure
-        episode1
-        enclosure
-        media:group
-        media:content
-        episode1
-        media:content
-        media:content
-        episode1
-        media:content
-        media:group
-      }.join('.*')}/
-    )
-  end
-
-  it "should not be able to combine 2 simple feeds on a filename with extension" do
-    @funnel_on_filename_with_extension.funnel(@other_feed).to_s.should_not match(
-      /#{%w{
-        enclosure
-        episode1
-        enclosure
-        media:group
-        media:content
-        episode1
-        media:content
-        media:content
-        episode1
-        media:content
-        media:group
-      }.join('.*')}/
-    )
+  
+  describe FeedFunnel::DirectMatchFunnel do
+    before do
+      @funnel_on_filename_with_extension    = FeedFunnel::DirectMatchFunnel.new(@master_feed) {|i| (i.h % :enclosure)[:url] }
+      @funnel_on_filename_without_extension = FeedFunnel::DirectMatchFunnel.new(@master_feed) {|i| (i.h % :enclosure)[:url].gsub(/\..*$/, "") }
+    end
+  
+    it "should be able to combine 2 simple feeds on a filename without extension" do
+      @funnel_on_filename_without_extension.funnel(@other_feed).to_s.should match(@combined_feed_regex)
+    end
+  
+    it "should not be able to combine 2 simple feeds on a filename with extension" do
+      @funnel_on_filename_with_extension.funnel(@other_feed).to_s.should_not match(@combined_feed_regex)
+    end
   end
 end
