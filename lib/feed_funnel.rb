f7bb82c1ff5811c7dc83006de505d5a436c23141
Justin Camerer <iamjwc@gmail.com>
2009-07-09 16:25:24 -0400

    Removed inline testing code

--- lib/feed_funnel.rb
+++ lib/feed_funnel.rb
@@ -11,10 +11,3 @@ require 'levenshtein_funnel'
 require 'direct_match_funnel'
 require 'date_proximity_funnel'
 
-# # Use publish date to determine if episodes are close at all.
-# M = FeedFunnel::Feed.new(File.read("spec/rss/super_simple_1.rss"))
-# c = FeedFunnel::LevenshteinFunnel.new(M) {|i| (i.h % :description).inner_text.gsub(/<[^>]*>/, "").gsub!(/\W+/, " ") }
-# 
-# F = FeedFunnel::Feed.new(File.read("spec/rss/super_simple_2.rss"))
-# D = c.funnel(F)
-
