b24f9b1d8f55b9e34503dee13b609d04f177db78
Justin Camerer <iamjwc@gmail.com>
2009-07-15 16:08:11 -0400

    Fixed similar items issue

--- lib/feed_funnel/funnel.rb
+++ lib/feed_funnel/funnel.rb
@@ -8,49 +8,43 @@ class FeedFunnel::Funnel
   end
 
   def GO!
-    self.preprocess
-    similar_items  = self.similar_items
-    all_feed_items = self.all_feed_items
+    @feeds.each do |feed|
+      self.preprocess(feed)
+      similar_items = self.similar_items(feed)
+      other_items = feed.items.dup
 
-    @master_feed.items.each do |item|
-      similar_items.keys.each do |similar_item|
-        item.add_media(similar_item.enclosure_values) if all_feed_items.include?(similar_item)
+      @master_feed.items.each do |item|
+        similar_items.keys.each do |similar_item|
+          item.add_media(similar_item.enclosure_values) if other_items.include?(similar_item)
 
-        # Get rid of items that have been associated with the master feed
-        all_feed_items.delete(similar_item)
+          # Get rid of items that have been associated with the master feed
+          other_items.delete(similar_item)
+        end
       end
+      other_items.each {|item| @master_feed.items << item }
     end
-    all_feed_items.each {|item| @master_feed.items << item }
 
     @master_feed
   end
 
   protected
 
-  def all_feed_items
-    @feeds.map {|feed| feed.items.dup }.flatten
-  end
-
-  def similar_items
+  def similar_items(feed)
     similar_items = {}
-    @feeds.each do |feed|
-      @matchers.each do |matcher|
-        @master_feed.items.each do |item|
-          items = matcher.similar_items(item, feed.items)
-          items.each do |i|
-            (similar_items[i] ||= []) << matcher
-          end
+    @matchers.each do |matcher|
+      @master_feed.items.each do |item|
+        items = matcher.similar_items(item, feed.items)
+        items.each do |i|
+          (similar_items[i] ||= []) << matcher
         end
       end
     end
     similar_items
   end
 
-  def preprocess
-    @feeds.each do |feed|
-      @matchers.each do |matcher|
-        matcher.preprocess(@master_feed, feed) if matcher.respond_to? :preprocess
-      end
+  def preprocess(feed)
+    @matchers.each do |matcher|
+      matcher.preprocess(@master_feed, feed) if matcher.respond_to? :preprocess
     end
   end
 end
